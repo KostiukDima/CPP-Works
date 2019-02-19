6c49ab3a9a4bc39cf26733704e5371930f7b5784
Dima Kostiuk <1dima.kostyk@gmail.com>
2019-02-19 11:53:46 +0200

    Pointer

---
+++ Class Works/19.02.2019/19.02.2019/Main.cpp
@@ -0,0 +1,115 @@
+#include<iostream>	
+#include<ctime>
+
+using namespace std;
+
+
+/*template<typename T1>
+void Product(T1 *pA, T1 *pB, T1 *pC, T1 *pSum )
+{
+	 *pSum=(*pA)*( *pB)*( *pC);	
+	 
+}
+
+template<typename T1, typename T2>
+void Arithmetic (T1 *pA, T1 *pB, T1 *pC, T2 *pSa)
+{
+	*pSa = (*pA* *pB* *pC) / 3;
+	
+}
+template<typename T1>
+void Min(T1 *pA, T1 *pB, T1 *pC, T1 *pMin)
+{
+	*pMin = *pA;
+	if (*pMin > *pB)
+	{
+		*pMin = *pB;
+	}
+	if (*pMin > *pC)
+	{
+		*pMin = *pC;
+	}
+	
+}
+
+
+int main()
+{
+	double a = 11.023, b = 0.25, c = 30.369,sum=0,min=0;
+	double *pA = &a, *pB = &b, *pC = &c, *pSum = & sum, *pMin = &min;
+	double sa = 0;
+	double *pSa = &sa;
+
+	Product(pA, pB, pC, pSum);
+	cout << "Sum = " << sum<<endl;
+
+	Arithmetic (pA, pB, pC, pSa);
+	cout << "Sa = " << sa << endl;
+
+	Min(pA, pB, pC,pMin);
+	cout << "Min = " << min << endl;
+
+	
+
+
+	system("pause");
+	return 0;
+}*/
+
+template<typename T1>
+void Sum(T1 *pA, T1 *pB, T1 *pSum);
+
+template<typename T1>
+void Difference(T1 *pA, T1 *pB, T1 *pSum);
+
+template<typename T1>
+void Product(T1 *pA, T1 *pB, T1 *pSum);
+
+template<typename T1>
+void Division(T1 *pA, T1 *pB, T1 *pSum);
+
+int main()
+{
+	double a = 10, b = 5, sum = 0;
+	double *pA = &a, *pB = &b, *pSum = &sum;
+	
+
+	Sum(pA, pB, pSum);
+	cout << "Sum = " << sum << endl;
+	
+	Difference(pA, pB, pSum);
+	cout << "Difference = " << sum << endl;
+
+	Product(pA, pB, pSum);
+	cout << "Product = " << sum << endl;
+
+	Division(pA, pB, pSum);
+	cout << "Division = " << sum << endl;
+
+	system("pause");
+	return 0;
+}
+
+template<typename T1>
+void Sum(T1 *pA, T1 *pB, T1 *pSum)
+{
+	*pSum = (*pA)+(*pB);
+}
+
+template<typename T1>
+void Difference(T1 *pA, T1 *pB, T1 *pSum)
+{
+	*pSum = (*pA) - (*pB);
+}
+
+template<typename T1>
+void Product(T1 *pA, T1 *pB, T1 *pSum)
+{
+	*pSum = (*pA) * (*pB);
+}
+
+template<typename T1>
+void Division(T1 *pA, T1 *pB, T1 *pSum)
+{
+	*pSum = (*pA) / (*pB);
+}
