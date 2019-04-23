10bb20b7a7615bd32177bf5c8386d5b167c5267f
Dima Kostiuk <1dima.kostyk@gmail.com>
2019-04-23 11:53:59 +0300

    OOP

---
+++ Class Works/23.04.2019/23.04.2019/Main.cpp
@@ -0,0 +1,85 @@
+#include<iostream>	
+#include<string>	
+
+using namespace std;
+
+//class Car
+//{
+//public:
+//	string model;
+//	float power;
+//	string color;
+//	int weight;
+//	string regnumber;
+//
+//	void ShowCarInfo()
+//	{
+//		cout << "Model\t\t" << model << "\nPower\t\t" << power << "\nColor\t\t" << color << "\nWeight\t\t" << weight << "\nRegnumber\t" << regnumber << endl;
+//	}
+//private:
+//
+//};
+//class Point
+//{
+//public:
+//	int x;
+//	int y;
+//};
+
+
+
+int main()
+{
+	/*Car mercedes;
+
+	mercedes.model = "A 140";
+	mercedes.power = 1.6;
+	mercedes.color = "Black";
+	mercedes.weight = 1200;
+	mercedes.regnumber = "BK 7777 AB";
+
+	mercedes.ShowCarInfo();
+
+	cout << endl;
+	
+	Car BMW;
+	BMW.model = "X5";
+	BMW.power = 2.5;
+	BMW.color = "Black";
+	BMW.weight = 1500;
+	BMW.regnumber = "BK 5555 AB";
+
+	BMW.ShowCarInfo();*/
+
+	/*Point Point;
+
+	cout << "Enter X\n";
+	cin >> Point.x;
+	
+	cout << "Enter Y\n";
+	cin >> Point.y;
+
+	cout << "Y = " << Point.y << "\nX = " << Point.x << endl;
+
+	system("cls");
+	for (int i = 0; i < Point.x; i++)
+	{
+		cout << "\n";
+	}
+	for (int i = 0; i < Point.y; i++)
+	{
+		cout << "  ";
+	}
+	cout << ".";
+
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;*/
+
+
+
+	system("pause");
+	return 0;
+}
